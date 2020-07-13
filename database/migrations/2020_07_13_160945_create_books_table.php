7aaf57c9016ee4a192fa536d9aecc5f4b3c5e86f
Dmitriy Hladyshev <dmglad7@gmail.com>
2020-07-13 20:32:38 +0300

    Book, Author, Genre entities were created

---
+++ database/migrations/2020_07_13_160945_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->year('year');
+            $table->integer('author')->unsigned();
+            $table->integer('genres')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
